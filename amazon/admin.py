bb569c38d818de27a574d2eca9abc9c479f11d11
Razi Ahmad <razi.ahmad@frnd.app>
2025-02-05 11:39:30 +0530

    add to admin

--- amazon/admin.py
+++ amazon/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from amazon.models import Seller, SellerCentralSale
+
+admin.site.register(Seller)
+admin.site.register(SellerCentralSale)
