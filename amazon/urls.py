3511b6cc7eb7664941d85b90005f2d04b70abc86
Razi Ahmad <razi.ahmad@frnd.app>
2025-02-12 23:00:55 +0530

    add fetch seller central data API endpoint to URL routing

--- amazon/urls.py
+++ amazon/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-from amazon.views import TryApi
+from amazon.views import TryApi, FetchSellerCentralDataAPI
+
 urlpatterns = [
     path("tryApi/", TryApi.as_view(), name="try-api"),
+    path("fetchData/", FetchSellerCentralDataAPI.as_view(), name="fetch-data"),
 ]
