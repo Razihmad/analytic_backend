9b8ceb8ef6fa5979e28099192eb278efeb3c8b83
Razi Ahmad <razi.ahmad@frnd.app>
2025-03-14 15:50:56 +0530

    refactor: add new endpoint for sales report retrieval in URL configuration

--- amazon/urls.py
+++ amazon/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from amazon.views import TryApi, FetchSellerCentralDataAPI
+from amazon.views import TryApi, FetchSellerCentralDataAPI, SalesAPI
 
 urlpatterns = [
     path("tryApi/", TryApi.as_view(), name="try-api"),
     path("fetchData/", FetchSellerCentralDataAPI.as_view(), name="fetch-data"),
+    path("getSalesReport/", SalesAPI.as_view(), name="get-sales-report"),
 ]
