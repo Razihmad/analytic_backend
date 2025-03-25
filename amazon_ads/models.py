4201c2ff48cac8c8298b49f261abaf0e34179fda
Razi Ahmad <razi.ahmad@frnd.app>
2025-03-26 00:19:08 +0530

    refactor: replace AmazonAdsAccount with Seller in AmazonAdsSaleAsin and AmazonAdsSaleCampaign models, and create migration for these changes

--- amazon_ads/models.py
+++ amazon_ads/models.py
@@ -1,27 +1,11 @@
 from django.db import models
-from django.contrib.auth.models import User
+from amazon.models import Seller
 
 # Create your models here.
 
 
-class AmazonAdsAccount(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    store_name = models.CharField(max_length=255)
-    refresh_token = models.CharField(max_length=500)
-    marketplace_id = models.CharField(max_length=255)
-    country_code = models.CharField(max_length=255)
-    currency_code = models.CharField(max_length=255)
-    amazon_seller_id = models.CharField(max_length=255)
-    profile_id = models.CharField(max_length=255)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        unique_together = ['user', 'amazon_seller_id', "marketplace_id"]
-
-
 class AmazonAdsSaleAsin(models.Model):
-    amazon_ads = models.ForeignKey(AmazonAdsAccount, on_delete=models.CASCADE)
+    amazon_ads = models.ForeignKey(Seller, on_delete=models.CASCADE)
     asin = models.CharField(max_length=255)
     sales_date = models.DateField()
     sales = models.DecimalField(max_digits=10, decimal_places=2)
@@ -39,7 +23,7 @@ class AmazonAdsSaleAsin(models.Model):
 
 
 class AmazonAdsSaleCampaign(models.Model):
-    amazon_ads = models.ForeignKey(AmazonAdsAccount, on_delete=models.CASCADE)
+    amazon_ads = models.ForeignKey(Seller, on_delete=models.CASCADE)
     campaign_name = models.CharField(max_length=255)
     campaign_id = models.CharField(max_length=255)
     sales_date = models.DateField()
