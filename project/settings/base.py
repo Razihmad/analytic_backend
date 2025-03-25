4186f3ad61886d31a976a5f16a2b828f4f577c4d
Razi Ahmad <razi.ahmad@frnd.app>
2025-03-25 13:07:17 +0530

    fix: reorder CORS middleware in settings to ensure proper functionality

--- project/settings/base.py
+++ project/settings/base.py
@@ -56,13 +56,13 @@ INSTALLED_APPS = DEFAULT_APPS + INTERNAL_APPS + EXTERNAL_APPS
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "corsheaders.middleware.CorsMiddleware",
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
