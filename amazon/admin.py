4dd2dbe2f99911bd2ad432c37a93cc35de2d3009
Razi Ahmad <razi.ahmad@frnd.app>
2025-03-29 01:13:21 +0530

    feat: register SellerCentralTraffic model in admin for better management of traffic data

--- amazon/admin.py
+++ amazon/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from amazon.models import Seller, SellerCentralSale, RegionDetail
+from amazon.models import Seller, SellerCentralSale, RegionDetail, SellerCentralTraffic
 
 admin.site.register(Seller)
 admin.site.register(SellerCentralSale)
 admin.site.register(RegionDetail)
+admin.site.register(SellerCentralTraffic)
