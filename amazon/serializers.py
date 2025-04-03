48500c9a6a3333f4884c2f3379898b4f0f673457
Razi Ahmad <razi.ahmad@frnd.app>
2025-04-03 16:18:26 +0530

    fix: round organic revenue calculation to two decimal places for improved accuracy in sales data processing

--- amazon/serializers.py
+++ amazon/serializers.py
@@ -97,7 +97,7 @@ def process_total_and_sales_data(*, total_sales: List[Dict], ads_sale: List[Dict
 
     total_sales_data["organic_orders"] = total_sales_data["total_orders"] - total_sales_data["ad_orders"]
     total_sales_data["organic_traffic"] = total_sales_data["total_traffic"] - total_sales_data["ad_traffic"]
-    total_sales_data["organic_revenue"] = float(total_sales_data["total_revenue"]) - float(total_sales_data["ads_revenue"])
+    total_sales_data["organic_revenue"] = round(float(total_sales_data["total_revenue"]) - float(total_sales_data["ads_revenue"]), 2)
 
     # calculate AOV
     if total_sales_data["total_orders"]:
