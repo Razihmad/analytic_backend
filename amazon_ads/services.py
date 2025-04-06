eaf36d1bf50f237e89d9e41ce7ca91585a8a2f0e
Razi Ahmad <razi.ahmad@frnd.app>
2025-04-06 17:29:19 +0530

    refactor: comment out async call to start_fetching_amazon_ads_by_date_range in services.py for future implementation

--- amazon_ads/services.py
+++ amazon_ads/services.py
@@ -107,18 +107,18 @@ def fetch_ads_data_by_date(*, user_id: int, amazon_seller_id: str, start_date: s
         raise ServiceException(f"no ads profile for {user_id=}, {amazon_seller_id=}")
     logger.info(f"{ads_profile=}")
     country_code = ads_profile.country_code
-    start_fetching_amazon_ads_by_date_range.apply_async(
-        args=[
-            amazon_seller_id,
-            start_date,
-            end_date,
-            country_code,
-            ads_profile.profile_id,
-            ads_profile.id,
-            user_id,
-        ],
-        queue="process_report"
-    )
+    # start_fetching_amazon_ads_by_date_range.apply_async(
+    #     args=[
+    #         amazon_seller_id,
+    #         start_date,
+    #         end_date,
+    #         country_code,
+    #         ads_profile.profile_id,
+    #         ads_profile.id,
+    #         user_id,
+    #     ],
+    #     queue="process_report"
+    # )
     start_fetching_amazon_ads_campaign_by_date_range.apply_async(
         args=[
             amazon_seller_id,
