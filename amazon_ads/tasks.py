26cf9e400ae7c54dbfec700930d267af0484fab1
Razi Ahmad <razi.ahmad@frnd.app>
2025-04-09 23:26:04 +0530

    refactor: update TryApi to process Sponsored Products reports with new report_id and URL; modify campaign_type and report_type for improved data handling

--- amazon_ads/tasks.py
+++ amazon_ads/tasks.py
@@ -224,14 +224,14 @@ def start_fetching_amazon_ads_campaign_by_date_range(
     ad_account_id: int,
     user_id: int
 ):
-    logger.info(f"start_fetching_amazon_ads_by_date_range, {start_date=}, {end_date=}, {ad_account_id=}, {profile_id=}")
+    logger.info(f"start_fetching_amazon_ads_campaign_date_range, {start_date=}, {end_date=}, {ad_account_id=}, {profile_id=}")
     start_date = dt.convert_str_to_date(date_str=start_date)
     end_date = dt.convert_str_to_date(date_str=end_date)
     region = get_region_by_country_code(country_code=country_code)
     access_token = get_ads_access_token(user_id=user_id, amazon_seller_id=amazon_seller_id, region=region)
     for campaign_type in AdProduct._member_names_:
         logger.info(f"[campaign type data] {amazon_seller_id=}, {start_date=}, {end_date=}, {profile_id=}, {campaign_type=}")
-        create_ads_campaign_data_report_by_date(
+        create_ads_campaign_data_report_by_date.apply_async(
             args=[
                 start_date  ,
                 end_date,
