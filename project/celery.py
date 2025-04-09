fed8e56c2375318e50355c81bba0ec9517dee460
Razi Ahmad <razi.ahmad@frnd.app>
2025-04-10 00:07:58 +0530

    refactor: remove unused logging configuration for Celery to streamline code and improve maintainability

--- project/celery.py
+++ project/celery.py
@@ -1,19 +1,10 @@
 import os
 from celery import Celery
-from celery.signals import setup_logging
 
 os.environ.setdefault(
     "DJANGO_SETTINGS_MODULE", os.getenv("DJANGO_SETTINGS_MODULE", default="project.settings")
 )
 app = Celery("project", broker=os.getenv("CELERY_BROKER_URL"))
-
-
-@setup_logging.connect
-def config_loggers(*args, **kwargs):
-    from logging.config import dictConfig
-    from django.conf import settings
-
-    dictConfig(settings.LOGGING)
 
 
 app.config_from_object("django.conf:settings", namespace="CELERY")
