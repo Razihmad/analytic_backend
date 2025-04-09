f1886bce9983acb0fec5b4c85eb0320100aa2cbf
Razi Ahmad <razi.ahmad@frnd.app>
2025-04-09 23:56:35 +0530

    feat: add logging configuration for Celery to enhance traceability and debugging capabilities

--- project/celery.py
+++ project/celery.py
@@ -1,5 +1,6 @@
 import os
 from celery import Celery
+from celery.signals import setup_logging
 
 os.environ.setdefault(
     "DJANGO_SETTINGS_MODULE", os.getenv("DJANGO_SETTINGS_MODULE", default="project.settings")
@@ -8,3 +9,11 @@ app = Celery("project", broker=os.getenv("CELERY_BROKER_URL"))
 app.config_from_object("django.conf:settings", namespace="CELERY")
 
 app.autodiscover_tasks()
+
+
+@setup_logging.connect
+def config_loggers(*args, **kwargs):
+    from logging.config import dictConfig
+    from django.conf import settings
+
+    dictConfig(settings.LOGGING)
