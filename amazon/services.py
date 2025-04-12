b582f588294e27224cf09337ca94050ae846dbce
Razi Ahmad <razi.ahmad@frnd.app>
2025-04-12 18:23:20 +0530

    fix: correct variable name from ads_spen to ads_spend in tacos and acos calculations for accurate sales metrics in get_asin_categorization_by_sales function

--- amazon/services.py
+++ amazon/services.py
@@ -246,8 +246,8 @@ def get_asin_categorization_by_sales(*, user_id: int, amazon_seller_id: int, sta
 
     for _, data in asin_wise_sales.items():
         data["cvr"] = round(data["orders"] / data["total_sessions"], 2) if data.get("total_sessions") else 0
-        data["tacos"] = round(data["ads_spen"] / data["sales"]) if data.get("sales") else 0
-        data["acos"] = round(data["ads_spen"] / data["ads_sales"]) if data.get("ads_sales") else 0
+        data["tacos"] = round(data["ads_spend"] / data["sales"]) if data.get("sales") else 0
+        data["acos"] = round(data["ads_spend"] / data["ads_sales"]) if data.get("ads_sales") else 0
         if data["sales"] > total_sales * 0.05:
             asin_greater_than_5_percent.append(data)
             tier_1_asins_count += 1
