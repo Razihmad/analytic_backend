81a81aadab399572f65ee6013e95e05f04222e27
Razi Ahmad <razi.ahmad@frnd.app>
2025-04-12 23:35:16 +0530

    fix: ensure accurate sales calculations in get_asin_categorization_by_sales function by converting sales and ads_sales to float before calculations for tacos and acos metrics

--- amazon/services.py
+++ amazon/services.py
@@ -245,12 +245,13 @@ def get_asin_categorization_by_sales(*, user_id: int, amazon_seller_id: int, sta
         asin_wise_sales[data["child_asin"]] = cur_data
 
     for asin, data in asin_wise_sales.items():
-        sales = data.get("sales")
-        ads_sales = data.get("ads_sales")
+        sales = float(data.get("sales"))
+        ads_sales = float(data.get("ads_sales"))
         logger.info(f"{sales=}, {ads_sales=}, {asin=}")
         data["cvr"] = round(float(data["orders"]) / float(data["total_sessions"]), 2) if data.get("total_sessions") else 0
-        data["tacos"] = round(float(data["ads_spend"]) / float(data["sales"])) if sales else 0
-        data["acos"] = round(float(data["ads_spend"]) / float(data["ads_sales"])) if ads_sales else 0
+        data["tacos"] = round(float(data["ads_spend"]) / sales) if sales else 0
+        data["acos"] = round(float(data["ads_spend"]) / ads_sales) if ads_sales else 0
+        logger.info(f"{data=}")
         if data["sales"] > total_sales * 0.05:
             asin_greater_than_5_percent.append(data)
             tier_1_asins_count += 1
