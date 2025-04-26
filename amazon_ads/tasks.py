6426fafc08a61ecceb35ac86300d20ffa24d5f78
Razi Ahmad <razi.ahmad@frnd.app>
2025-04-26 21:53:25 +0530

    fix: improve logging in download_file_and_process_report_data function to include campaign type for enhanced traceability

--- amazon_ads/tasks.py
+++ amazon_ads/tasks.py
@@ -133,6 +133,7 @@ def download_file_and_process_report_data(user_id: int, report_id: str, url: str
         logger.info(f"report data upserted, {user_id=}, {report_id=} {ad_account_id=}, {report_type=}")
         return
     if report_type in [AdsReportTypeId.SP_CAMPAIGN.value, AdsReportTypeId.SD_CAMPAING.value, AdsReportTypeId.SB_CAMPAIGN.value]:
+        logger.info(f"processing data {user_id=}, {report_type=}, {campaign_type=}")
         data = group_ad_sales_by_campaign(sales=data, campaign_type=campaign_type)
         logger.info(f"{user_id=}, {report_id=}, {ad_account_id=}, {len(data)=}")
         data = prepare_campaing_level_data_for_upsert(data=data, ad_account_id=ad_account_id, campaign_type=campaign_type)
