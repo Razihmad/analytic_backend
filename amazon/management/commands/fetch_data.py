5b9cc8fa290587127e584539e2d169ffcb99f54a
Razi <mail2razi4u@gmail.com>
2025-07-03 14:41:25 +0530

    fix: convert start_date to string format for consistency in data fetching in fetch_data command

--- amazon/management/commands/fetch_data.py
+++ amazon/management/commands/fetch_data.py
@@ -10,7 +10,7 @@ class Command(BaseCommand):
     help = "Fetch data from Amazon"
 
     def handle(self, *args, **options):
-        start_date = dt.now(with_tz=True).date() - dt.timedelta(days=1)
+        start_date = str(dt.now(with_tz=True).date() - dt.timedelta(days=1))
         end_date = start_date
         seller = Seller.objects.get(id=26)
         amazon_seller_id = seller.amazon_seller_id
@@ -19,7 +19,7 @@ class Command(BaseCommand):
         profile_id = seller.profile_id
 
         fetch_sales_report_by_date_range.apply_async(
-            args=[seller.user_id, seller.amazon_seller_id, str(start_date), str(start_date)], queue="process_report",
+            args=[seller.user_id, seller.amazon_seller_id, start_date, start_date], queue="process_report",
         )
 
         start_fetching_amazon_ads_by_date_range.apply_async(
