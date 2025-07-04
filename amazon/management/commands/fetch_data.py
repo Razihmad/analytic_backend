b9c018f6849ef25cd97b1cf25b74267cbd864a68
Razi <mail2razi4u@gmail.com>
2025-07-04 07:58:09 +0530

    fix: update date range in fetch_data command to correctly fetch reports for the last 8 days

--- amazon/management/commands/fetch_data.py
+++ amazon/management/commands/fetch_data.py
@@ -10,8 +10,8 @@ class Command(BaseCommand):
     help = "Fetch data from Amazon"
 
     def handle(self, *args, **options):
-        start_date = str(dt.now(with_tz=True).date() - dt.timedelta(days=1))
-        end_date = start_date
+        start_date = dt.now(with_tz=True).date() - dt.timedelta(days=1)
+        end_date = dt.now(with_tz=True).date() - dt.timedelta(days=8)
         seller = Seller.objects.get(id=26)
         amazon_seller_id = seller.amazon_seller_id
         country_code = seller.country_code
@@ -19,14 +19,14 @@ class Command(BaseCommand):
         profile_id = seller.profile_id
 
         fetch_sales_report_by_date_range.apply_async(
-            args=[seller.user_id, seller.amazon_seller_id, start_date, start_date], queue="process_report",
+            args=[seller.user_id, seller.amazon_seller_id, str(start_date), str(end_date)], queue="process_report",
         )
 
         start_fetching_amazon_ads_by_date_range.apply_async(
             args=[
                 amazon_seller_id,
-                start_date,
-                end_date,
+                str(start_date),
+                str(end_date),
                 country_code,
                 profile_id,
                 seller.id,
@@ -37,8 +37,8 @@ class Command(BaseCommand):
         start_fetching_amazon_ads_campaign_by_date_range.apply_async(
             args=[
                 amazon_seller_id,
-                start_date,
-                end_date,
+                str(start_date),
+                str(end_date),
                 country_code,
                 profile_id,
                 seller.id,
@@ -49,8 +49,8 @@ class Command(BaseCommand):
         start_fetching_search_term_report.apply_async(
             args=[
                 amazon_seller_id,
-                start_date,
-                end_date,
+                str(start_date),
+                str(end_date),
                 country_code,
                 profile_id,
                 seller.id,
