a1bf7da06a132c4ca41ad54e60e4da2737d1e235
Razi <mail2razi4u@gmail.com>
2025-07-07 17:59:19 +0530

    fix: change sales and spends values to integers in get_search_term_aggregated_data for consistent data representation

--- amazon_ads/services.py
+++ amazon_ads/services.py
@@ -283,9 +283,9 @@ def get_search_term_aggregated_data(*, search_term_data: List[Dict]) -> Dict:
     return {
         "impressions": impressions,
         "clicks": clicks,
-        "sales": round(sales, 2),
+        "sales": int(sales),
         "orders": orders,
-        "spends": round(spends, 2),
+        "spends": int(spends),
         "acos": round(100 * spends / sales, 2) if sales > 0 else 0,
         "roas": round(sales / spends, 2) if spends > 0 else 0,
         "ctr": round(100 * clicks / impressions, 2) if impressions > 0 else 0,
