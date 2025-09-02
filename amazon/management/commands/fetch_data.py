8d024795b9451222f411d997a6abd5447e34c67a
Razi <mail2razi4u@gmail.com>
2025-09-02 21:04:40 +0530

    refactor: update fetch_data command to handle multiple sellers, improving data retrieval for sales and ads reports

--- amazon/management/commands/fetch_data.py
+++ amazon/management/commands/fetch_data.py
@@ -12,61 +12,62 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         start_date = dt.now(with_tz=True).date() - dt.timedelta(days=8)
         end_date = dt.now(with_tz=True).date() - dt.timedelta(days=1)
-        seller = Seller.objects.get(id=26)
-        amazon_seller_id = seller.amazon_seller_id
-        country_code = seller.country_code
-        user_id = seller.user_id
-        profile_id = seller.profile_id
+        sellers = Seller.objects.filter(id__in=[26, 35])
+        for seller in sellers:
+            amazon_seller_id = seller.amazon_seller_id
+            country_code = seller.country_code
+            user_id = seller.user_id
+            profile_id = seller.profile_id
 
-        fetch_sales_report_by_date_range.apply_async(
-            args=[seller.user_id, seller.amazon_seller_id, str(start_date), str(end_date)], queue="process_report",
-        )
+            fetch_sales_report_by_date_range.apply_async(
+                args=[seller.user_id, seller.amazon_seller_id, str(start_date), str(end_date)], queue="process_report",
+            )
 
-        start_fetching_amazon_ads_by_date_range.apply_async(
-            args=[
-                amazon_seller_id,
-                str(start_date),
-                str(end_date),
-                country_code,
-                profile_id,
-                seller.id,
-                user_id,
-            ],
-            queue="process_ads_report"
-        )
-        start_fetching_amazon_ads_campaign_by_date_range.apply_async(
-            args=[
-                amazon_seller_id,
-                str(start_date),
-                str(end_date),
-                country_code,
-                profile_id,
-                seller.id,
-                user_id,
-            ],
-            queue="process_ads_report"
-        )
-        start_fetching_search_term_report.apply_async(
-            args=[
-                amazon_seller_id,
-                str(start_date),
-                str(end_date),
-                country_code,
-                profile_id,
-                seller.id,
-                user_id,
-            ],
-            queue="process_ads_report"
-        )
-        start_fetching_targeting_report.apply_async(
-            args=[
-                amazon_seller_id,
-                str(start_date),
-                str(end_date),
-                country_code,
-                seller.profile_id,
-                seller.id,
-                user_id,
-            ],
-            queue="process_ads_report"
-        )
+            start_fetching_amazon_ads_by_date_range.apply_async(
+                args=[
+                    amazon_seller_id,
+                    str(start_date),
+                    str(end_date),
+                    country_code,
+                    profile_id,
+                    seller.id,
+                    user_id,
+                ],
+                queue="process_ads_report"
+            )
+            start_fetching_amazon_ads_campaign_by_date_range.apply_async(
+                args=[
+                    amazon_seller_id,
+                    str(start_date),
+                    str(end_date),
+                    country_code,
+                    profile_id,
+                    seller.id,
+                    user_id,
+                ],
+                queue="process_ads_report"
+            )
+            start_fetching_search_term_report.apply_async(
+                args=[
+                    amazon_seller_id,
+                    str(start_date),
+                    str(end_date),
+                    country_code,
+                    profile_id,
+                    seller.id,
+                    user_id,
+                ],
+                queue="process_ads_report"
+            )
+            start_fetching_targeting_report.apply_async(
+                args=[
+                    amazon_seller_id,
+                    str(start_date),
+                    str(end_date),
+                    country_code,
+                    seller.profile_id,
+                    seller.id,
+                    user_id,
+                ],
+                queue="process_ads_report"
+            )
