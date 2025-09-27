48a62fbd5413f176a031cd43e71e72ad6dc42b8b
Razi <mail2razi4u@gmail.com>
2025-09-27 22:27:22 +0530

    fix: modify key access in get_asin_categorization_by_sales function to consistently use group_by parameter, improving data aggregation for sales and ads metrics

--- amazon/services.py
+++ amazon/services.py
@@ -300,7 +300,7 @@ def get_asin_categorization_by_sales(
     asin_wise_sales = defaultdict(dict)
     for data in total_sales_data:
         cur_data = asin_wise_sales[data[group_by]]
-        ads_sales, ads_spend = get_ad_sales_by_asin(ads_sales_by_asin=ads_sales_data, asin=data["child_asin"])
+        ads_sales, ads_spend = get_ad_sales_by_asin(ads_sales_by_asin=ads_sales_data, asin=data[group_by])
         cur_data["ads_sales"] = int(ads_sales)
         cur_data["ads_spend"] = int(ads_spend)
         total_sessions, sku = get_sessions_and_sku_of_asin(traffic_data=total_traffic_data, asin=data["child_asin"])
@@ -309,20 +309,20 @@ def get_asin_categorization_by_sales(
         cur_data["sales"] = cur_data.get("sales", 0) + data["sales"]
         cur_data["orders"] = cur_data.get("orders", 0) + data["orders"]
         cur_data["units_ordered"] = data["units_ordered"] + cur_data.get("units_ordered", 0)
-        cur_data["asin"] = data["child_asin"]
-        asin_wise_sales[data["child_asin"]] = cur_data
+        cur_data[group_by] = data[group_by]
+        asin_wise_sales[data[group_by]] = cur_data
 
     for data in prev_total_sales_data:
         cur_data = asin_wise_sales[data[group_by]]
-        prev_ads_sales, prev_ads_spend = get_ad_sales_by_asin(ads_sales_by_asin=prev_ads_sales_data, asin=data["child_asin"])
+        prev_ads_sales, prev_ads_spend = get_ad_sales_by_asin(ads_sales_by_asin=prev_ads_sales_data, asin=data[group_by])
         cur_data["prev_ads_sales"] = int(prev_ads_sales)
         cur_data["prev_ads_spend"] = int(prev_ads_spend)
-        prev_total_sessions, _ = get_sessions_and_sku_of_asin(traffic_data=prev_total_traffic_data, asin=data["child_asin"])
+        prev_total_sessions, _ = get_sessions_and_sku_of_asin(traffic_data=prev_total_traffic_data, asin=data[group_by])
         cur_data["prev_total_sessions"] = prev_total_sessions + cur_data.get("prev_total_sessions", 0)
         cur_data["prev_sales"] = cur_data.get("prev_sales", 0) + data["sales"]
         cur_data["prev_orders"] = cur_data.get("prev_orders", 0) + data["orders"]
         cur_data["prev_units_ordered"] = data["units_ordered"] + cur_data.get("prev_units_ordered", 0)
-        asin_wise_sales[data["child_asin"]] = cur_data
+        asin_wise_sales[data[group_by]] = cur_data
 
     # tier_1_sales, tier_2_sales, tier_3_sales = 0, 0, 0
     for _, data in asin_wise_sales.items():
